75de3701b06993dc6a94c9c1e0a6c89091dd67ad
Pierre Volet <volet@msn.com>
2017-05-31 23:29:30 +0200

    Working content area deployement

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
 		
+		var leftWidth = $('.contentLeft').width();
+				
 		$('#button').on('click', function(){
 			$('.right').css({'height':"2px"});
-			$('.right').animate({'width':"100%"},800);
+			$('.right').animate({'width':"100%"},500);
 			
 			$('.left').css({'height':"2px"});
-			$('.left').animate({'width':"100%"},800);
+			$('.left').animate({'width':"100%"},500, function() {
+    			$('.content').css({'backgroundColor':"#848484"});
+    			$('.right').animate({'height':"400px"}, 800)
+    			$('.left').animate({'height':"400px"}, 800, function(){
+    				$('.texte').fadeIn();
+    		
+    			});
+  			});
+			
     	});
-		
-		
-	});
+    	
+   });
